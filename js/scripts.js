e320a51e92a0bdf1d0e957d6e066a46dc893de97
Piotrek <mydifolt@gmail.com>
2017-05-23 20:38:41 +0200

    2nd commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 add.addEventListener('click', function(e) {
   var element = document.createElement('li');
-  element.innerHTML = 'item';
   var liElements = document.getElementsByTagName('li')
   var lengthOfLi = liElements.length;
+  element.innerHTML = 'item'+ lengthOfLi ;
   list.appendChild(element);
 });
